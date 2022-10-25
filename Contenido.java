5f526cb6ba6bf3241eb3b0d5d92b0673991188d0
USUARIOPC <f.galarza2003@hotmail.com>
2022-10-25 11:55:01 -0500

    tarea de introduccion a la programacion

---
+++ Contenido.java
@@ -0,0 +1,24 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("Receta para hacer un minipastel de oreo");
+        System.out.println("Ingredientes:");
+        System.out.println("Un paquete de galletas oreo");
+        System.out.println("Leche, harina, cocoa y polvo de hornear");
+        System.out.println("Pasos:");
+        System.out.println("1.Colocar 4 oreos en una taza");
+        System.out.println("2.Colocar 1/4 de taza de leche");
+        System.out.println("3.Colocar una cucharada de cocoa");
+        System.out.println("4.Colocar una cucharada de harina");
+        System.out.println("5.Colocar una pizca de polvo de hornear");
+        System.out.println("6.Sumergir almendras troceadas");
+        System.out.println("7.Meter en el microondas por 1 minuto");
+        System.out.println("8.Dejar que se enfríe por 1 minuto más");
+        System.out.println("Receta para hacer un batido de oreo con leche y banana fácil y rápido");
+        System.out.println("Pasos:");
+        System.out.println("1.Colocar 6 oreos dentro de la licuadora");
+        System.out.println("2.Colocar una banana");
+        System.out.println("3.Colocar 3 tazas de leche");
+        System.out.println("Licuar");
+        System.out.println("Y listo, ya puedes disfrutar de estas dos recetas sencillas y sobre todo deliciosas");
+    }
+}
