7e8ca7fc8788fbc89899df4cea28424dc57bf108
USUARIOPC <f.galarza2003@hotmail.com>
2022-10-25 12:46:30 -0500

    tarea de introduccion a la programing

--- Git.java
+++ Git.java
@@ -1,8 +1,9 @@
 public class Git {
     public static void main(String[] args) {
-        System.out.println("git init creará un nuevo repositorio local GIT");
-        System.out.println("git add se usa para agregar archivos al área de preparación");
-        System.out.println("git commit creará una instantánea de los cambios y la guardará en el directorio git");
+        System.out.println("Comandos Git");
+        System.out.println("git init creara un nuevo repositorio local GIT");
+        System.out.println("git add se usa para agregar archivos al área de preparacion");
+        System.out.println("git commit creara una instantanea de los cambios y la guardará en el directorio git");
         System.out.println("git branch se usa para listar, crear o borrar ramas");
         System.out.println("git remote te permite ver todos los repositorios remotos");
         System.out.println("git push se usa para enviar confirmaciones locales a la rama maestra del repositorio remoto");
