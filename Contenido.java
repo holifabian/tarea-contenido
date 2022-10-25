2c206e71e9b4940b755658158ce1b3fad362fcd9
USUARIOPC <f.galarza2003@hotmail.com>
2022-10-25 12:18:28 -0500

    tarea de introduccion a la programacion

--- Contenido.java
+++ Contenido.java
@@ -1,24 +1,24 @@
 public class Contenido {
     public static void main(String[] args) {
-        System.out.println("Receta para hacer un minipastel de oreo");
-        System.out.println("Ingredientes:");
-        System.out.println("Un paquete de galletas oreo");
-        System.out.println("Leche, harina, cocoa y polvo de hornear");
-        System.out.println("Pasos:");
-        System.out.println("1.Colocar 4 oreos en una taza");
-        System.out.println("2.Colocar 1/4 de taza de leche");
-        System.out.println("3.Colocar una cucharada de cocoa");
-        System.out.println("4.Colocar una cucharada de harina");
-        System.out.println("5.Colocar una pizca de polvo de hornear");
-        System.out.println("6.Sumergir almendras troceadas");
-        System.out.println("7.Meter en el microondas por 1 minuto");
-        System.out.println("8.Dejar que se enfríe por 1 minuto más");
-        System.out.println("Receta para hacer un batido de oreo con leche y banana fácil y rápido");
-        System.out.println("Pasos:");
-        System.out.println("1.Colocar 6 oreos dentro de la licuadora");
-        System.out.println("2.Colocar una banana");
-        System.out.println("3.Colocar 3 tazas de leche");
-        System.out.println("Licuar");
-        System.out.println("Y listo, ya puedes disfrutar de estas dos recetas sencillas y sobre todo deliciosas");
+        System.out.println("Algoritmo: es básicamente un conjunto de pasos para una determinada tarea");
+        System.out.println("Bootstrap: framework CSS");
+        System.out.println("Código: conjunto de palabras o símbolos que contienen instrucciones para la computadora");
+        System.out.println("Queries: solicitud de información y/o datos para una base de datos.");
+        System.out.println("Full-stack: unión de Front-End y Back-End");
+        System.out.println("Backup: copia de archivos en otro dispositivo para guardar informacion");
+        System.out.println("Bandwidth: ancho de banda. Dice cuánto se puede viajar en una red");
+        System.out.println("Big Data: almacenamiento de grandes cantidades de datos");
+        System.out.println("Bit: abreviatura de binary digit");
+        System.out.println("Byte: unión de 8 bits");
+        System.out.println("Tipado: significa una variable o algún valor que tiene un tipo predeterminado");
+        System.out.println("Clase: proyecto de todo objeto");
+        System.out.println("Instancia: la ejecución de una clase");
+        System.out.println("Objeto: la ejecución de una clase");
+        System.out.println("Herencia: capacidad de heredar las características de otra clase");
+        System.out.println("Miembro de Clase: puede ser una función o una propiedad");
+        System.out.println("SuperClase: la clase padre; clase que se amplía");
+        System.out.println("SubClase: la clase hija; la clase que parte de la implementación de otra");
+        System.out.println("Dev. Team: equipo de desarrollo");
+        System.out.println("React: biblioteca JavaScript para crear el Front-End de aplicaciones");
     }
 }
